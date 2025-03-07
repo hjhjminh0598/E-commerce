54ff8ee88f08ab9a4b65a25f62cd28c99baf248a
giangnt <giangnt@falcongames.com>
2025-03-07 18:26:02 +0700

    Add Spring Cloud LoadBalancer with Eureka

--- product-service/src/main/java/com/ecom/user/ProductApplication.java
+++ product-service/src/main/java/com/ecom/user/ProductApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableFeignClients
+@SpringBootApplication
 public class ProductApplication {
 
     public static void main(String[] args) {
